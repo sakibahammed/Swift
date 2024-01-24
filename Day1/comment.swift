054ee177b5569bb97e4043e360c9bca540982479
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-01-25 01:46:33 +0600

    some basic swift programme

---
+++ Day1/comment.swift
@@ -0,0 +1,11 @@
+// single line comment
+
+/*
+    multiline comment for swift
+
+    Woohoo! you have written a few swift programmes
+    In this lession, you have learned some basic things here
+
+
+
+*/
---
+++ Day1/output.swift
@@ -0,0 +1,10 @@
+print("Hello")
+print("Goodbye")
+
+// print pattern 
+
+
+print("      1")
+print("    2 3")
+print("  4 5 6")
+print("7 8 9 10")
