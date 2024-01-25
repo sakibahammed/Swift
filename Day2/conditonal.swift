18ab85fe37cc10421c35930263d605781920c53d
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-01-26 04:31:04 +0600

    conditional statement in swift

---
+++ Day2/conditonal.swift
@@ -0,0 +1,12 @@
+var  learningToCode: Bool = true;
+
+if(learningToCode){
+    print("Don't forget to take breaks! You got this.")
+}
+
+
+
+var loggedIn: Bool = true;
+if(loggedIn){
+    print("welcome")
+}
