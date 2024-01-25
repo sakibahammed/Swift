51cae5c6a0ff8b8399f142828af1986e6b8b501d
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-01-26 03:11:09 +0600

    type in Swift

--- Day1/tyoee.swift
+++ Day1/tyoee.swift
@@ -13,3 +13,12 @@ var name = true;
 
 var age: Int = 123;
 
+var name: String = "Tuku pakhi";
+
+var age: Int = 23;
+
+var add: String = "Badda";
+var dinnerDone: Bool = False;
+
+
+var weight: Double = 123.6243;
