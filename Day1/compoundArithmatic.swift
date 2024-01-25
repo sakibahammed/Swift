141251577e4a999ef363e28a796b0e739b3a6fb4
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-01-26 02:51:25 +0600

    compound arithmatic operators

---
+++ Day1/compoundArithmatic.swift
@@ -0,0 +1,40 @@
+var age = 99;
+
+age = age+1;
+// print(age)
+
+var price = 4;
+price += 4;
+
+price -= 1;
+
+price *= 2
+
+
+price /= 2
+
+
+price %= 2
+
+print(price);
+
+//  fruit stall
+
+
+var apples = 16
+
+// a customer comes buys 4 
+
+apples -= 4;
+
+// another customer comes and buys half of the apple
+
+
+apples /= 2
+
+print(apples)
+
+
+
+
+
