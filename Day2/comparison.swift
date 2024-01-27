de6e173a219c47f003b6cd3f161374212f8b7ff2
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-01-27 14:03:41 +0600

    comparison

--- Day2/comparison.swift
+++ Day2/comparison.swift
@@ -1,40 +1,63 @@
-/*
-    == equal to
-    != Not equal to 
-    > greater than
-    < less than
-    >= greater than or equal to
-    <= less than or equal to
+// /*
+//     == equal to
+//     != Not equal to 
+//     > greater than
+//     < less than
+//     >= greater than or equal to
+//     <= less than or equal to
 
 
-*/
+// */
 
 
-// print(4<5)
-// print(0.5>0.1)
-// print(3.5<=3.0)
-// print(12>=15)
-// print("a" == "a")
-// print("B" != "b")
+// // print(4<5)
+// // print(0.5>0.1)
+// // print(3.5<=3.0)
+// // print(12>=15)
+// // print("a" == "a")
+// // print("B" != "b")
 
 
-let grade = 95;
-if grade>65{
-    print("You Pass")
+// let grade = 95;
+// if grade>65{
+//     print("You Pass")
+// }
+// else{
+//     print("You fail")
+// }
+
+
+// let tuneSquadPoints: Int = 78;
+
+// let monstarsPoint: Int = 77
+
+// if(tuneSquadPoints<monstarsPoint){
+//     print("\(tuneSquadPoints) wins")
+// }
+
+// else{
+//     print("\(monstarsPoint) wins")
+// }
+
+
+
+// if , else if , else
+
+
+let grade: Int = 80
+
+let letterGrade: String;
+
+if(grade>=90){
+    print("You are passed , You get 'A' ")
+}
+else if(grade>81 && grade<=89){
+    print("You got 'B' ")
+    
 }
 else{
-    print("You fail")
+    print("You passed")
+    
 }
 
-
-let tuneSquadPoints: Int = 78;
-
-let monstarsPoint: Int = 77
-
-if(tuneSquadPoints<monstarsPoint){
-    print("\(tuneSquadPoints) wins")
-}
-
-else{
-    print("\(monstarsPoint) wins")
-}
+// print(letterGrade)
