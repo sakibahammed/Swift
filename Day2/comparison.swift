abd6da5a1f5ae62ed305d750ac8d5c6d5ab06505
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-01-27 13:56:41 +0600

    basic if-else in comparison

--- Day2/comparison.swift
+++ Day2/comparison.swift
@@ -10,9 +10,31 @@
 */
 
 
-print(4<5)
-print(0.5>0.1)
-print(3.5<=3.0)
-print(12>=15)
-print("a" == "a")
-print("B" != "b")
+// print(4<5)
+// print(0.5>0.1)
+// print(3.5<=3.0)
+// print(12>=15)
+// print("a" == "a")
+// print("B" != "b")
+
+
+let grade = 95;
+if grade>65{
+    print("You Pass")
+}
+else{
+    print("You fail")
+}
+
+
+let tuneSquadPoints: Int = 78;
+
+let monstarsPoint: Int = 77
+
+if(tuneSquadPoints<monstarsPoint){
+    print("\(tuneSquadPoints) wins")
+}
+
+else{
+    print("\(monstarsPoint) wins")
+}
