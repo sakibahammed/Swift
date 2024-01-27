7d877bb015f67c95b6f248ec50fa591cf78da6fb
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-01-27 12:09:07 +0600

    if-else present

--- Day2/conditonal.swift
+++ Day2/conditonal.swift
@@ -10,3 +10,6 @@ var loggedIn: Bool = true;
 if(loggedIn){
     print("welcome")
 }
+
+
+
---
+++ Day2/ifElse.swift
@@ -0,0 +1,10 @@
+var isPresent: Bool = false;
+
+if(isPresent){
+    print("I will be present in the class");
+     
+}
+
+else{
+    print("I am absent");
+}
