bb7dd7eb45c4e6a813d90b10e7c4438d21e5a573
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-01-27 13:05:44 +0600

    comparison in swift

---
+++ Day2/comparison.swift
@@ -0,0 +1,18 @@
+/*
+    == equal to
+    != Not equal to 
+    > greater than
+    < less than
+    >= greater than or equal to
+    <= less than or equal to
+
+
+*/
+
+
+print(4<5)
+print(0.5>0.1)
+print(3.5<=3.0)
+print(12>=15)
+print("a" == "a")
+print("B" != "b")
