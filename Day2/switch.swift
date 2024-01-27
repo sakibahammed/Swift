ab0b2cf1bf36747787f4644e6136f68904456f1c
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-01-28 00:30:50 +0600

    switch case

--- Day2/switch.swift
+++ Day2/switch.swift
@@ -1,8 +1,32 @@
 //switch operator
 
 
-var city: String = "Rome";
+var city: String = "rome";
 
-if(city == "Rome"){
-  
-}
+// if(city == "Rome"){
+//   print("Moai")
+// }
+// else if(city == "New york"){
+//     print("Welcome tho new york")
+// }
+
+// else{
+//     print("Welcome to anywhere you are now")   
+// }
+
+
+
+switch city{
+    case "rome":
+        print("Moai");
+        break;
+    case "dhaka":
+        print("moeye moeye");
+        break;
+    case "sylt":
+        print("Moeye moeye moeye");
+        break;
+
+    default:
+        print("Moeyx");
+}   
