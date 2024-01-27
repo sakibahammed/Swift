fca913979d90c8848c60c5e485a586f3e6d91c38
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-01-27 15:44:54 +0600

    ternary operator

---
+++ Day2/ternaryOperator.swift
@@ -0,0 +1,22 @@
+// if-else in short
+
+// Regular Expresssion
+
+let orderSuccessFullyPlaced: Bool = false;
+
+
+// if(orderSuccessFullyPlaced){
+//     print("Your Order is received");
+// }
+
+// else{
+//     print("Something went wrong");
+// }
+
+
+
+
+
+//ternary operator
+
+orderSuccessFullyPlaced ? print("Your order was received"): print("Something went wrong");
