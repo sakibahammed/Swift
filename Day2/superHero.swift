486dfad97bb5a30e03eedee5892df93a771a363b
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-01-28 01:01:16 +0600

    switch

---
+++ Day2/superHero.swift
@@ -0,0 +1,19 @@
+var secretIdentity = "Tony Stark";
+var superheroName: String;
+
+
+switch(secretIdentity){
+    case "Tony Stark":
+        superheroName = "Iron Man";
+
+    case "Natasha Romanoff":
+        superheroName = "Black Widow";
+    case "Prince T'Challa":
+        superheroName = "Black Panther";
+    case "Thor":
+        superheroName = "Thor";
+    default:
+        superheroName = "Unknown";
+}
+
+
